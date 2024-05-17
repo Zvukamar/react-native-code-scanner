fb1ab3a3f781895742cb008aa4ad74f3ef362a4a
Alex <fre3run@gmail.com>
2024-05-17 17:19:17 +0300

    fixed worklet crash

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,6 +2,7 @@ import { iOSCorners } from '@mgcrea/vision-camera-barcode-scanner';
 import { HORIZONTAL_PERCENTAGE_MARGIN, VERTICAL_PERCENTAGE_MARGIN } from './consts';
 
 export const iOSresultInCenter = (corners: iOSCorners) => {
+    'worklet';
     const { bottomLeft, bottomRight, topLeft } = corners;
     if (bottomLeft.x >= HORIZONTAL_PERCENTAGE_MARGIN && bottomRight.x <= 1 - HORIZONTAL_PERCENTAGE_MARGIN) {
         return bottomLeft.y >= VERTICAL_PERCENTAGE_MARGIN && topLeft.y <= 1 - VERTICAL_PERCENTAGE_MARGIN;
